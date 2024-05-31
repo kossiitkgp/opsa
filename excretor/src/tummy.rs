8814364d9a48a6eaabb70ce356559787e9d85bb4
Devansh-bit <devanshg308@gmail.com>
2024-05-31 19:30:52 +0530

    fix: reversed order again

--- excretor/src/tummy.rs
+++ excretor/src/tummy.rs
@@ -115,8 +115,8 @@ impl Tummy {
                     WHERE channel_id = $1
                     GROUP BY join_ts, parent_user_id
                 ) as c ON messages.ts = c.join_ts AND messages.user_id = c.parent_user_id
-                WHERE channel_id = $1 AND ts < $2 AND messages.parent_user_id = ''
-                ORDER BY ts DESC LIMIT $3
+                WHERE channel_id = $1 AND ts > $2 AND messages.parent_user_id = ''
+                ORDER BY ts ASC LIMIT $3
                 "#,
                 channel_id,
                 timestamp,
@@ -138,7 +138,7 @@ impl Tummy {
                     GROUP BY join_ts, parent_user_id
                 ) as c ON messages.ts = c.join_ts AND messages.user_id = c.parent_user_id
                 WHERE channel_id = $1 AND messages.parent_user_id = ''
-                ORDER BY ts DESC LIMIT $2
+                ORDER BY ts ASC LIMIT $2
 	            ",
                 channel_id,
                 *msgs_per_page as i64
