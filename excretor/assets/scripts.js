1bd3dbabaa2b3b0aa4d47ce3eab3cdbc82f0a078
Chirag Ghosh <cghosh828049@gmail.com>
2024-06-02 20:15:00 +0530

    feat: show messages since a particular date
    Signed-off-by: Chirag Ghosh <cghosh828049@gmail.com>

---
+++ excretor/assets/scripts.js
@@ -0,0 +1,36 @@
+const DateTime = easepick.DateTime;
+
+htmx.onLoad(function () {
+    const datePicker = document.getElementById("date-picker");
+    const picker = new easepick.create({
+        element: datePicker,
+        css: [
+            "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css",
+        ],
+        zIndex: 10,
+        plugins: ["AmpPlugin"],
+        AmpPlugin: {
+            dropdown: {
+                months: true,
+                years: true,
+            },
+            darkMode: false,
+        },
+        setup(picker) {
+            picker.on("select", (e) => {
+                const channelID = datePicker.getAttribute("data-channel-id");
+                const selectedDate = new DateTime(e.detail.date).format(
+                    "YYYY-MM-DD HH:mm:ss",
+                );
+                htmx.ajax(
+                    "GET",
+                    `/messages/${channelID}?per_page=10&since=${selectedDate}`,
+                    {
+                        target: ".message-container",
+                        swap: "innerHTML",
+                    },
+                );
+            });
+        },
+    });
+});
