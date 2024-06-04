5b26cd6a4c56c7aaf9c28a0357acef44604867ca
Chirag Ghosh <cghosh828049@gmail.com>
2024-06-04 11:30:47 +0530

    feat: create middleware for checking cookie
    Signed-off-by: Chirag Ghosh <cghosh828049@gmail.com>

--- excretor/src/routing.rs
+++ excretor/src/routing.rs
@@ -1,5 +1,62 @@
 use crate::{env::EnvVars, tummy::Tummy};
-use axum::{routing::get, Router};
+use axum::{body::Body, extract::{Request, State}, http::StatusCode, middleware::{self, Next}, response::Response, routing::get, Router};
+use axum_extra::extract::cookie::CookieJar;
+use hmac::{Hmac, Mac};
+use jwt::VerifyWithKey;
+use reqwest::Client;
+use sha2::Sha256;
+use std::collections::BTreeMap;
+
+const FORBIDDEN_MSG: &str = "Mortals are forbidden from accessing the site";
+
+async fn verify_token(token: &String, state: &RouterState) -> Result<bool, handlers::AppError> {
+    // verify the jwt token and accessing slack auth test api
+    let key: Hmac<Sha256> =
+        Hmac::new_from_slice(state.env_vars.slack_signing_secret.as_bytes()).unwrap();
+    let claims: BTreeMap<String, String> = token.verify_with_key(&key)?;
+    let user_id = claims.get("user_id").unwrap();
+    let access_token = claims.get("access_token").unwrap();
+
+    let slack_auth_test_url = "https://slack.com/api/auth.test";
+    let req = Client::new()
+        .get(slack_auth_test_url)
+        .header("Authorization", format!("Bearer {}", access_token))
+        .build()?;
+    let response = Client::new().execute(req).await?;
+
+    if response.status() != StatusCode::OK {
+        return Ok(false);
+    }
+
+    let user = state.tummy.get_user_info(user_id).await?;
+    if user.id.is_empty() || user.is_bot || user.deleted {
+        return Ok(false);
+    }
+
+    Ok(true)
+}
+
+async fn verify_token_middleware(State(state): State<RouterState>, jar: CookieJar, request: Request, next: Next) -> Result<Response, handlers::AppError> {
+    if state.env_vars.slack_auth_enable {
+        if let Some(token) = jar.get("token").map(|cookie| cookie.value().to_owned()) {
+            let is_verified = verify_token(&token, &state).await?;
+            if !is_verified {
+                return Ok(Response::builder()
+                            .status(StatusCode::UNAUTHORIZED)
+                            .body(Body::from(FORBIDDEN_MSG))
+                            .unwrap());
+            }
+        } else {
+            return Ok(Response::builder()
+                        .status(StatusCode::TEMPORARY_REDIRECT)
+                        .header("Location", "/login")
+                        .body(Body::empty())
+                        .unwrap());
+        }
+    }
+
+    Ok(next.run(request).await)
+}
 
 #[derive(Clone)]
 struct RouterState {
@@ -8,22 +65,26 @@ struct RouterState {
 }
 
 pub fn get_excretor_router(tummy: Tummy, env_vars: EnvVars) -> Router {
+    let state = RouterState { tummy, env_vars };
+
     Router::new()
         // `GET /` goes to `root`
         .route("/", get(handlers::root))
         .route("/channels/:channel_name", get(handlers::load_channel))
         .route("/messages/:channel_id", get(handlers::get_messages))
         .route("/fallback-avatar", get(handlers::fallback_avatar))
-        .route("/assets/*file", get(handlers::assets))
         .route("/replies", get(handlers::get_replies))
+        .route_layer(middleware::from_fn_with_state(state.clone(), verify_token_middleware))
         .route("/auth", get(handlers::auth))
         .route("/auth/callback", get(handlers::auth_callback))
         .route("/login", get(handlers::login))
-        .with_state(RouterState { tummy, env_vars })
+        .route("/assets/*file", get(handlers::assets))
+        .with_state(state)
 }
 
 mod handlers {
     use super::RouterState;
+    use super::FORBIDDEN_MSG;
     use crate::templates;
     use crate::tummy::SlackDateTime;
     use askama::Template;
@@ -37,7 +98,7 @@ mod handlers {
     use axum_extra::extract::cookie::{CookieJar, Cookie};
     use cookie::time::Duration;
     use hmac::{Hmac, Mac};
-    use jwt::{SignWithKey, VerifyWithKey};
+    use jwt::SignWithKey;
     use reqwest::Client;
     use serde::Deserialize;
     use sha2::Sha256;
@@ -93,31 +154,9 @@ mod handlers {
         code: String,
     }
 
-    const FORBIDDEN_MSG: &str = "Mortals are forbidden from accessing the site";
-
     pub(super) async fn root(
         State(state): State<RouterState>,
-        jar: CookieJar,
     ) -> Result<(StatusCode, Response), AppError> {
-        if state.env_vars.slack_auth_enable {
-            if let Some(token) = jar.get("token").map(|cookie| cookie.value().to_owned()) {
-                let is_verified = verify_token(&token, &state).await?;
-                if !is_verified {
-                    return Ok((
-                        StatusCode::UNAUTHORIZED,
-                        Body::from(FORBIDDEN_MSG).into_response(),
-                    ));
-                }
-            } else {
-                return Ok((
-                    StatusCode::FOUND,
-                    Response::builder()
-                        .header("Location", "/login")
-                        .body(Body::empty())
-                        .unwrap(),
-                ));
-            }
-        }
 
         let channels = state.tummy.get_all_channels().await?;
 
@@ -331,33 +370,6 @@ mod handlers {
                 .body(Body::empty())
                 .unwrap(),
         ))
-    }
-
-    async fn verify_token(token: &String, state: &RouterState) -> Result<bool, AppError> {
-        // verify the jwt token and accessing slack auth test api
-        let key: Hmac<Sha256> =
-            Hmac::new_from_slice(state.env_vars.slack_signing_secret.as_bytes()).unwrap();
-        let claims: BTreeMap<String, String> = token.verify_with_key(&key)?;
-        let user_id = claims.get("user_id").unwrap();
-        let access_token = claims.get("access_token").unwrap();
-
-        let slack_auth_test_url = "https://slack.com/api/auth.test";
-        let req = Client::new()
-            .get(slack_auth_test_url)
-            .header("Authorization", format!("Bearer {}", access_token))
-            .build()?;
-        let response = Client::new().execute(req).await?;
-
-        if response.status() != StatusCode::OK {
-            return Ok(false);
-        }
-
-        let user = state.tummy.get_user_info(user_id).await?;
-        if user.id.is_empty() || user.is_bot || user.deleted {
-            return Ok(false);
-        }
-
-        Ok(true)
     }
 
     pub(super) async fn login() -> Result<(StatusCode, Response), AppError> {
