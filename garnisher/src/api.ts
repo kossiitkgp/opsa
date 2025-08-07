1ca2d4e39be87647b620e01d2a7de47b03dee747
Devansh-bit <devanshg308@gmail.com>
2025-08-07 23:20:04 +0530

    fix: remove unused API_BASE_URL

--- garnisher/src/api.ts
+++ garnisher/src/api.ts
@@ -1,10 +1,9 @@
-const API_BASE_URL: string = ''; // Use an empty string for relative paths
 export const API_ENDPOINTS = {
-    channels: `${API_BASE_URL}/api/channels`,
-    channelAndMessages: (channelId: string) => `${API_BASE_URL}/api/channels/${channelId}`,
+    channels: `/api/channels`,
+    channelAndMessages: (channelId: string) => `/api/channels/${channelId}`,
     messages: (channelId: string, beforeTimestamp: string | null) =>
-        `${API_BASE_URL}/api/messages/${channelId}${beforeTimestamp ? `?before_msg_timestamp=${beforeTimestamp}&per_page=20` : '?per_page=20'}`,
+        `/api/messages/${channelId}${beforeTimestamp ? `?before_msg_timestamp=${beforeTimestamp}&per_page=20` : '?per_page=20'}`,
     replies: (ts: number, userId: string, channelId: string) =>
-        `${API_BASE_URL}/api/replies?ts=${ts}&user_id=${userId}&channel_id=${channelId}`,
-    search: `${API_BASE_URL}/api/search`,
+        `/api/replies?ts=${ts}&user_id=${userId}&channel_id=${channelId}`,
+    search: `/api/search`,
 };
